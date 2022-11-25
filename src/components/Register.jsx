bdbadd081b50493b3b25db465d28f2e88965d207
TH-Raju <rajukhan894200@gmail.com>
2022-11-25 11:38:46 +0600

    jwt set to localstorage

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -64,11 +64,23 @@ const Register = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                getUserToken(email);
                 setCreatedUserEmail(email);
 
             })
 
+    }
+
+
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken);
+                    navigate('/');
+                }
+            })
     }
 
 
