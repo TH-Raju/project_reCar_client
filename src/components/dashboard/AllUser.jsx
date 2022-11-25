44b5fa91e24b5443c22bcdb7c0fccd0b4054c533
TH-Raju <rajukhan894200@gmail.com>
2022-11-25 15:30:56 +0600

    make admin route setup and implement

--- src/components/dashboard/AllUser.jsx
+++ src/components/dashboard/AllUser.jsx
@@ -1,9 +1,63 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUser = () => {
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users');
+            const data = await res.json();
+            return data;
+        }
+    });
+
+
+
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Make admin successful');
+                    refetch();
+                }
+            })
+    }
+
     return (
         <div>
             <h2 className="text-4xl">All User</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, i) => <tr key={user._id} className="hover">
+                                <th>{i + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user?.role !== 'Admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-primary'>Make Admin</button>}</td>
+                                <td><button className='btn btn-warning'>Delete</button></td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
