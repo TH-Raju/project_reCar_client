d51747d049f37ae175e659296e780ed792b02e73
TH-Raju <rajukhan894200@gmail.com>
2023-03-18 16:08:20 +0600

    new feature added

--- src/components/Home/Choose.jsx
+++ src/components/Home/Choose.jsx
@@ -4,7 +4,7 @@ const Choose = () => {
     return (
         <div className='mb-36 w-5/6 mx-auto'>
             <h2 className="text-5xl text-center font-bold my-4 mb-16">Why choose us?</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 bg-gray-300 p-10 rounded-xl'>
                 <div className="card w-96 bg-base-100 shadow-xl">
                     <figure className="px-10 pt-10">
                         <img src={require('../../img/certify.png')} alt="brands" className="rounded-xl w-12 h-12" />
