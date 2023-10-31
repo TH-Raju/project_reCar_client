21b9ced6836557d80cb4100289836ab4f0145a8a
TH-Raju <rajukhan894200@gmail.com>
2023-10-31 20:58:50 +0600

    Desing chage

--- src/components/Home/InfoSection.jsx
+++ src/components/Home/InfoSection.jsx
@@ -12,42 +12,42 @@ const InfoSection = () => {
             <h2 className='text-center text-3xl font-bold'>About ReCar</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:mx-12 my-16'>
                 <div className="card card-side bg-base-100 shadow-xl">
-                    <figure><img src={shop} className='h-16 w-80 lg:w-36 lg:h-20 -mt-28' alt="shop" /></figure>
+                    <figure><img src={shop} className='h-16 w-80 lg:w-48 lg:h-20 -mt-28' alt="shop" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Shop</h2>
                         <p>Find cars for sale, local dealers, and advice. Also, our price-badging and price-drop notifications keep you informed of potential deals.</p>
                         <div className="card-actions justify-end">
-                            <Link to='/products'><button className="btn btn-primary">Find Cars</button></Link>
+                            <Link to='/products'><button className="btn btn-primary btn-sm">Find Cars</button></Link>
                         </div>
                     </div>
                 </div>
                 <div className="card card-side bg-base-100 shadow-xl">
-                    <figure><img src={news} className='h-16 w-72 lg:w-36 lg:h-20 -mt-28' alt="news" /></figure>
+                    <figure><img src={news} className='h-16 w-72 lg:w-32 lg:h-14 -mt-28' alt="news" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">News</h2>
                         <p>Our very own resale handing experts offer unbiased coverage of today's automotive landscape. Yes, there will be dad jokes.</p>
                         <div className="card-actions justify-end">
-                            <Link to='/products'><button className="btn btn-primary">Find Cars</button></Link>
+                            <Link to='/products'><button className="btn btn-primary btn-sm">Find Cars</button></Link>
                         </div>
                     </div>
                 </div>
                 <div className="card card-side bg-base-100 shadow-xl">
-                    <figure><img src={sell} className='h-16 w-72 lg:w-48 lg:h-20 -mt-28' alt="sell" /></figure>
+                    <figure><img src={sell} className='h-16 w-72 lg:w-56 lg:h-16 -mt-28' alt="sell" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Sell</h2>
                         <p>Check out our free, secure methods for selling your car. You can either sell directly to a private-party shopper or get an Instant Offer from a dealership. Choose the option that’s right for you!</p>
                         <div className="card-actions justify-end">
-                            <Link to='/products'><button className="btn btn-primary">Find Cars</button></Link>
+                            <Link to='/products'><button className="btn btn-primary btn-sm">Find Cars</button></Link>
                         </div>
                     </div>
                 </div>
                 <div className="card card-side bg-base-100 shadow-xl">
-                    <figure><img src={service} className='h-16 w-72 lg:w-36 lg:h-20 -mt-28' alt="service" /></figure>
+                    <figure><img src={service} className='h-16 w-72 lg:w-44 lg:h-14 -mt-28' alt="service" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Serice and Repair</h2>
                         <p>You can find Service and Repair on your buying product if your seller want to give it you.if service and Repair available on those product you can find it from those product owner</p>
                         <div className="card-actions justify-end">
-                            <Link to='/products'><button className="btn btn-primary">Find Cars</button></Link>
+                            <Link to='/products'><button className="btn btn-primary btn-sm">Find Cars</button></Link>
                         </div>
                     </div>
                 </div>
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,63 +1,130 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/AuthProvider';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext);
-    const handleLogOut = () => {
-        logOut()
-            .then(() => { })
-            .catch(err => console.log(err))
-    }
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
 
-    const menuItems = <React.Fragment>
-        <li className='font-bold text-lg'><Link to="/home">Home</Link></li>
-        <li className='font-bold text-lg'><Link to="/products">Products</Link></li>
-        <li className='font-bold text-lg'><Link to="/blog">Blog</Link></li>
-        <li className='font-bold text-lg'><Link to="/faq">FAQ</Link></li>
-        {
-            user?.uid ?
-                <>
-                    <li className='font-bold text-lg'><Link to="/dashboard">Dashboard</Link></li>
-                    <li><button className='btn btn-outline btn-sm mt-3 px-2 py-0 rounded-lg' onClick={handleLogOut}>Log Out</button></li>
-                </>
-                :
-                <>
-                    <li><Link to="/login"><button className='btn btn-outline btn-sm mt-2 px-2 py-0 rounded-lg'>Log in</button></Link></li>
-                    <li><Link to="/register"><button className='btn btn-outline btn-sm mt-2 px-2 py-0 rounded-lg'>Register</button></Link></li>
-                </>
-        }
-
+  const menuItems = (
+    <React.Fragment>
+      <li className="font-bold text-md">
+        <Link to="/home">Home</Link>
+      </li>
+      <li className="font-bold text-md">
+        <Link to="/products">Products</Link>
+      </li>
+      <li className="font-bold text-md">
+        <Link to="/blog">Blog</Link>
+      </li>
+      <li className="font-bold text-md">
+        <Link to="/faq">FAQ</Link>
+      </li>
+      {user?.uid ? (
+        <>
+          <li className="font-bold text-md">
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <button
+              className="btn btn-outline btn-sm mt-2 px-2 py-0 rounded-lg"
+              onClick={handleLogOut}
+            >
+              Log Out
+            </button>
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/login">
+              <button className="btn btn-outline btn-sm mt-2 px-2 py-0 rounded-lg">
+                Log in
+              </button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+              <button className="btn btn-outline btn-sm mt-2 px-2 py-0 rounded-lg">
+                Register
+              </button>
+            </Link>
+          </li>
+        </>
+      )}
     </React.Fragment>
-    return (
-        <div className='sticky top-0 z-30'>
-            <div className="navbar bg-base-100 shadow-slate-300 shadow-md flex lg:justify-around">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {menuItems}
-                        </ul>
-                    </div>
-                    <div className='flex items-center'>
-                        <img src={require("../../img/resale.png")} className="h-6 w-6 -mr-3" alt="" />
-                        <Link to='/home' className="btn btn-ghost normal-case text-xl font-bold">ReCar</Link>
-                    </div>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal p-0">
-
-                        {menuItems}
-
-                    </ul>
-                </div>
-                <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden navbar-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-            </div>
+  );
+  return (
+    <div className="sticky top-0 z-30">
+      <div className="navbar bg-base-100 shadow-slate-300 shadow-md flex lg:justify-around">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={1}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 "
+            >
+              {menuItems}
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <img
+              src={require("../../img/resale.png")}
+              className="h-6 w-6 -mr-3"
+              alt=""
+            />
+            <Link
+              to="/home"
+              className="btn btn-ghost normal-case text-xl font-bold"
+            >
+              ReCar
+            </Link>
+          </div>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0 ">{menuItems}</ul>
+        </div>
+        <label
+          htmlFor="dashboard-drawer"
+          tabIndex={2}
+          className="btn btn-ghost lg:hidden navbar-end"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
